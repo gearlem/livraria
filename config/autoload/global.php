0acfc6be56fd98f52f132163f85108e4ea438c9f
gearlem <gearlem@gmail.com>
2013-08-18 21:20:25 -0300

    add primeiro commit

---
+++ config/autoload/global.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'db' => array(
+        'driver' => 'Pdo',
+        'dsn'    => 'mysql:dbname=zf2_livraria;host=localhost',
+        'driver_options' => array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF-8\''
+        )
+    )
+);
